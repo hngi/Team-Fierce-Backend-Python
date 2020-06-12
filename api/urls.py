12660402641c75108aac70cd092fc697baf4031b
Topnics <topwhiz5@gmail.com>
2020-06-12 17:04:40 +0100

    pdated views and urls under the API folder
    Updated views and urls under the API folder

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sendmail/', views.SendMail.as_view()),
-    path('sendmailwithtemplate/', views.SendMailWithTemplate.as_view())
-]
